3171402428f31cd4968698dd7fde218cb9c96922
iTitou <moiandme@gmail.com>
2020-01-25 18:36:47 +0100

    [fix] No default for SELF_QUERY_URL
    Signed-off-by: iTitou <moiandme@gmail.com>

--- settings.py
+++ settings.py
@@ -4,20 +4,20 @@ from os import environ as env
 REDIS_URL: str = env.get('REDIS_URL', 'redis://localhost')
 
 # The Slack application OAuth2 client ID
-SLACK_CLIENT_ID: str = env.get('SLACK_CLIENT_ID', "")
+SLACK_CLIENT_ID: str = env.get('SLACK_CLIENT_ID', '')
 
 # The Slack application OAuth2 client secret
-SLACK_CLIENT_SECRET: str = env.get('SLACK_CLIENT_SECRET', "")
+SLACK_CLIENT_SECRET: str = env.get('SLACK_CLIENT_SECRET', '')
 
 # The Slack signing secret, used to authenticate incoming Slack events
 # https://api.slack.com/docs/verifying-requests-from-slack
-SLACK_SIGN_SECRET: str = env.get('SLACK_SIGN_SECRET', "")
+SLACK_SIGN_SECRET: str = env.get('SLACK_SIGN_SECRET', '')
 
 # This is used to run the app on Heroku with a free plan, where the web
 # server is put to sleep if it receives no requests for 30minutes.
 # We therefore make this app query itself every 5 minutes to keep the
 # web server alive. Put the full URL to the /ping endpoint
-SELF_QUERY_URL: str = env.get('SELF_QUERY_URL', 'http://localhost:8000/ping')
+SELF_QUERY_URL: str = env.get('SELF_QUERY_URL', '')
 
 # If true, use mocked responses instead of the Deliveroo API
 USE_MOCK: bool = 'USE_MOCK' in env
